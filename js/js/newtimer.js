71d3cf39f286151751ae424e143b17d785c724f7
Acehoodvelox <89309940+Acehoodvelox@users.noreply.github.com>
2021-09-23 13:30:06 +0100

    Update newtimer.js

--- js/js/newtimer.js
+++ js/js/newtimer.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Sep 20, 2021 00:00:00").getTime();
+var countDownDate = new Date("Sep 27, 2021 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
